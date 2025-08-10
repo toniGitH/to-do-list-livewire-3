9930e2e675ce3298da73da6760852a3fd12dd1ee
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-08-10 21:18:23 +0200

    Declaradas propiedades fillable en el modelo Task y establecida relación uno a muchos con TaskList.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,16 @@ class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'done_at',
+        'task_list_id'
+    ];
+
+    public function list()
+    {
+        return $this->belongsTo(TaskList::class);
+    }
 }
--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -19,4 +19,9 @@ class TaskList extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
