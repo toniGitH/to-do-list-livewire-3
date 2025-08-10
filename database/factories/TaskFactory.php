01ff7f3011dec775bfb8013d1d4c8ae5e69421b7
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-08-10 21:17:16 +0200

    Definición de las reglas del TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\TaskList;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->unique()->sentence(rand(3,4), false),
+            'description' => $this->faker->description(),
+            'done_at' => null,
+            'task_list_id' => TaskList::factory()
         ];
     }
 }
