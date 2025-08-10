ce630382da780a195771ee6a7542e8a006af9416
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-08-10 23:36:55 +0200

    Añadido enlace a Tasks en vista sidebar.blade

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,6 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="rectangle-stack" :href="route('tasks.index')" :current="request()->routeIs('tasks.*')" wire:navigate>{{ __('Tasks') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
