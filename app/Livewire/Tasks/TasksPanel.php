0e338c9eb5dc8da74b2ae3cbdebd97841573f035
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-08-18 12:57:50 +0200

    Creado nuevo componente livewire 3 TasksPanel (clase y vista) En este componente se gestionarán sólo las tareas de cada lista

---
+++ app/Livewire/Tasks/TasksPanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Tasks;
+
+use Livewire\Component;
+
+class TasksPanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.tasks.tasks-panel');
+    }
+}
